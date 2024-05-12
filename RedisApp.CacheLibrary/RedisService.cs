ebc59bbcc1c2b2392244227904c1aa6950afa904
fatih serhat turan <95492309+fatihserhatturan@users.noreply.github.com>
2024-05-12 17:10:23 +0300

    Add files via upload

---
+++ RedisApp.CacheLibrary/RedisService.cs
@@ -0,0 +1,24 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedisApp.CacheLibrary
+{
+    public class RedisService
+    {
+        private readonly ConnectionMultiplexer _redis;
+
+        public RedisService(string url)
+        {
+            _redis = ConnectionMultiplexer.Connect(url);
+        }
+
+        public IDatabase GetDb(int db)
+        {
+            return _redis.GetDatabase(db);
+        }
+    }
+}
